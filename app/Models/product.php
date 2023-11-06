f6c969fb8d6c73fd0a064a7c271795ff24db54ee
Tazin Sultana <tazimsultana457@gmail.com>
2023-11-06 18:28:08 +0600

    Database Created

--- app/Models/product.php
+++ app/Models/product.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'price',
+        'description'
+    ];
 }
---
+++ resources/views/Category/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+</head>
+
+<body>
+
+
+
+
+
+    @include('Category.addModal')
+    @include('Category.CategoryAjax')
+    @include('Category.updatemodal')
+</body>
+
+</html>
